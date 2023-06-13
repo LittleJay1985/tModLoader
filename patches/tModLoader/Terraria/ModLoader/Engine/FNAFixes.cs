9fa383393cc7f68d1e379c3d73ff0e260fbd626c
LittleJay1985 <John@Whitehead.pro>
2023-06-13 20:32:47 +0100

    Re-enable opengl & vulkan EnvVar on Windows
    FNA3D now supports directx, opengl and vulkan on windows.
    Disable the SDL_VIDEODRIVER override when not set to directx on windows.

--- patches/tModLoader/Terraria/ModLoader/Engine/FNAFixes.cs
+++ patches/tModLoader/Terraria/ModLoader/Engine/FNAFixes.cs
@@ -11,12 +11,13 @@ internal static class FNAFixes
 			// FNA sets this to "1" on Windows. Terraria does not want this. See #2020
 			SDL.SDL_SetHint(SDL.SDL_HINT_VIDEO_MINIMIZE_ON_FOCUS_LOSS, "0");
 
+			// FNA3D now supports directx, opengl and vulkan on windows
 			// only directx is supported. See #2926
-			var videoDriver = Environment.GetEnvironmentVariable("SDL_VIDEODRIVER");
-			if (videoDriver != null && videoDriver != "directx") {
-				Logging.FNA.Debug("Cleared SDL_VIDEODRIVER=" + videoDriver);
-				Environment.SetEnvironmentVariable("SDL_VIDEODRIVER", null);
-			}
+			//var videoDriver = Environment.GetEnvironmentVariable("SDL_VIDEODRIVER");
+			//if (videoDriver != null && videoDriver != "directx") {
+			//	Logging.FNA.Debug("Cleared SDL_VIDEODRIVER=" + videoDriver);
+			//	Environment.SetEnvironmentVariable("SDL_VIDEODRIVER", null);
+			//}
 		}
 	}
 }
